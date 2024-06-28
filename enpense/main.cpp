37503b62daf490a31e75e50c2b2e8a0525dc7fe2
KirsaliAditya <kirsaliaditya@gmail.com>
2024-06-28 14:01:07 +0530

    initial commit

---
+++ enpense/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+/*
+* > CMD Args...
+* - CMD:
+* - add     Label (+/- default: -) 0.00
+* - del		Label
+* - list
+* - eval
+* - exit
+*/
+
+int main()
+{
+	cout << "Hello World" << endl;
+}
